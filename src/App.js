6c545b62762aebf0afda9c4ac0044baae8520982
haravindreddyrajula <haravindreddyrajula@gmail.com>
2021-05-28 22:30:10 +0530

    Firebase deployement done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
-import "./App.css";
 import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
@@ -11,6 +13,12 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
           </Route>
         </Switch>
       </Router>
